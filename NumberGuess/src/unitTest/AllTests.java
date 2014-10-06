951df9a42e6fc1435bd513a2c5730364b3874f75
al-saffar <anders_alsaffar@hotmail.com>
2014-10-06 22:30:37 +0200

    1

--- NumberGuess/src/unitTest/AllTests.java
+++ NumberGuess/src/unitTest/AllTests.java
@@ -1,8 +1,5 @@
 package unitTest;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,14 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ DatabaseTest.class, GameTest.class, SQLTest.class, UserTest.class })
-public class AllTests extends TestCase {
+public class AllTests{
 	
-	public static Test suite() {   
-		  TestSuite suite = new TestSuite("Test for test..");     
-		   suite.addTestSuite(DatabaseTest.class);
-		   suite.addTestSuite(GameTest.class);
-		   suite.addTestSuite(SQLTest.class);
-		   suite.addTestSuite(UserTest.class);
-		  return suite;   
-		 }
+	
 	}
